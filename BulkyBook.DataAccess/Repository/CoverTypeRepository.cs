0fe0f3e73f19859472c6917b4e38aea57d142f18
alejorom <94083673+alejorom@users.noreply.github.com>
2023-02-09 16:52:17 -0500

    Agregar CoverType DA/Model/Controler

---
+++ BulkyBook.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private ApplicationDBContext _context;
+
+        public CoverTypeRepository(ApplicationDBContext context) : base(context)
+        {
+			_context = context;
+        }
+
+        public void Update(CoverType obj)
+        {
+			_context.CoverTypes.Update(obj);
+        }
+    }
+}
