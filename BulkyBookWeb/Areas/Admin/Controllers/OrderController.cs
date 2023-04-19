e21702f4d65eee5a78e07b83b88af43c542a3968
alejorom <94083673+alejorom@users.noreply.github.com>
2023-04-19 12:53:09 -0500

    Adicionar Order Management

---
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+            orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            return Json(new { data = orderHeaders });
+        }
+
+        #endregion
+
+    }
+}
